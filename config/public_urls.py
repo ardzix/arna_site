9369c156051b51b523aa640454919360bc7eef26
Aqila Bintang Muzafa <147041734+aqilamuzafa917@users.noreply.github.com>
2026-04-09 00:54:57 +0700

    fix:swagger permissions

--- config/public_urls.py
+++ config/public_urls.py
@@ -12,7 +12,7 @@ schema_view = get_schema_view(
    ),
    public=True,
    urlconf='config.public_urls',
-   permission_classes=(permissions.IsAuthenticated,),
+   permission_classes=(permissions.AllowAny,),
 )
 
 urlpatterns = [
